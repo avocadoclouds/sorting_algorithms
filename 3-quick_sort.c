1708d55b26b6822b466ac7aadfb440df309fa540
avocadoclouds <rehana447@gmail.com>
2022-01-09 14:05:11 -0800

    add

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -42,8 +42,9 @@ int partition(int array[], int low, int high)
 
 	int pivot = array[high];
 	int i = (low - 1);
+	int j;
 
-	for (int j = low; j < high; j++)
+	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -68,8 +69,6 @@ void quickSort_recursion(int array[], int low, int high)
 {
 	if (low < high)
 	{
-		int pivot;
-
 		if (low < high)
 		{
 			int pivot = partition(array, low, high);
