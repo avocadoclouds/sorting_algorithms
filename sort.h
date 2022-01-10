3d4afeb9b896742c0e458478b9be83138589c224
Samie <samrasolomon40@gmail.com>
2022-01-10 10:38:55 +0000

    Added files

--- sort.h
+++ sort.h
@@ -32,5 +32,6 @@ void shell_sort(int *array, size_t size);
 void swap(int *a, int *b);
 int partition(int array[], int low, int high);
 void quickSort_recursion(int array[], int low, int high);
-
+int max_number (int *array, size_t size);
+void counting_sort(int *array, size_t size);
 #endif
